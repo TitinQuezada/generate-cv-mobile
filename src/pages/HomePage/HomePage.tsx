6b1ad9f2f9c87d10748bec5b89c618e042510476
Titin <jtitin@ri.gob.do>
2022-05-21 20:51:19 -0400

    React native paper added
    Vector icons added
    home page added

---
+++ src/pages/HomePage/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export const HomePage = () => {
+  return (
+    <View style={{padding: 25}}>
+      <Text>Home page</Text>
+    </View>
+  );
+};
