faaa2ae65bc7cc62269baf8f82e1085170e565f1
Titin <jtitin@ri.gob.do>
2022-05-21 21:37:51 -0400

    i18n translate added

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {HomePage} from './src/pages/HomePage/HomePage';
 import {Provider as PaperProvider} from 'react-native-paper';
+import './assets/i18n/i18n';
 
 const App = () => {
   return (
